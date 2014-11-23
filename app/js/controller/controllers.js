589bed2b3639244209207abebb03c66cb0efee53
sawyerlin <sawyer.lin@gmail.com>
2014-11-23 22:29:26 +0100

    Integrate home.json

--- app/js/controller/controllers.js
+++ app/js/controller/controllers.js
@@ -1,11 +1,19 @@
 var galleryApp = angular.module('galleryApp', []);
 
-galleryApp.controller('HomeController', function($scope) {
-  $scope.painters= [
-  {name: 'davinci'},
-  {name: 'picasso'},
-  {name: 'monet'},
-  {name: 'vangogh'},
-  {name: 'xubeihong'},
-  ];
+galleryApp.directive('onLastRepeat', function () {
+  return function (scope, element, attrs) {
+    if (scope.$last) setTimeout(function () {
+      scope.$emit('onRepeatLast', element, attrs);
+    }, 1);
+  
+  }
+}).controller('HomeController', function($scope, $http) {
+  $scope.$on('onRepeatLast', function(scope, element, attrs) {
+    $('#top').unslider();
+  });
+
+  $http.get('datas/home.json').success(function(data) {
+    $scope.TopFive= data.TopFive;
+    $scope.Painters = data.Painters;
+  });
 });
