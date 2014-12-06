93bc1685b83fb45f289f0e4332faa3a7e21f1098
sawyerlin <sawyer.lin@gmail.com>
2014-12-06 23:01:23 +0100

    add contact, detail views

--- js/controller/controllers.js
+++ js/controller/controllers.js
@@ -1,16 +1,7 @@
-var galleryApp = angular.module('galleryApp', []);
+var galleryControllers = angular.module('galleryControllers', []);
 
-galleryApp.directive('onLastRepeat', function () {
-  return function (scope, element, attrs) {
-    if (scope.$last) setTimeout(function () {
-      scope.$emit('onRepeatLast', element, attrs);
-    }, 1);
-  }
-}).controller('HomeController', function($scope, $http) {
-  $scope.$on('onRepeatLast', function(scope, element, attrs) {
-    $('#carousel').carousel();
-  });
-
+galleryControllers.controller('HomeController', function($scope, $http) {
+  
   $http.get('datas/home.json').success(function(data) {
     $scope.TopFive = data.TopFive;
     $scope.Kind = data.Kind;
@@ -19,18 +10,58 @@ galleryApp.directive('onLastRepeat', function () {
     $scope.ChangeFilter = function (filter) {
       $scope.Filter = filter;
     };
+    $scope.Limits = [];
+    $scope.ChangeLimit = function (name) {
+      var painter = _.find($scope.Painters, function(painter) { return painter.Name === name; });
+
+      painter.Limit = painter.Limit == 5 ? 100 : 5;
+      painter.LimitName = painter.Limit == 5 ? 'Encore Plus': 'Moins';
+    }
 
     angular.forEach (data.Painters, function (value, key) {
       $http.get('datas/' + value + '.json').success(function(data) {
-        data.Oeuvre= data.Oeuvre.slice(0, 6);
         var painter = {
           Name : value,
-          Contents : data
+          Contents : data,
+          Limit: 5,
+          LimitName: 'Encore Plus'
         };
         $scope.Painters.push(painter);
       });
     });
   });
-}).controller('DetailController', function($scope, $http) {
+}).controller('DetailController', function($scope, $routeParams, $http) {
+    var name = $routeParams.name,
+        id = $routeParams.id;
+    
+    $http.get('datas/' + name + '.json').success(function(data) {
+      var oeuvre = _.find(data.Oeuvre, function(oeuvre) { return oeuvre.Id == id; });
+      $scope.Oeuvre = oeuvre;
+    });
+}).controller('ContactController', function($scope) {
+  function setPosition(mapOptions) {
+    console.log(mapOptions);
+    console.log(google.maps);
+    var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+    console.log(map);
+  }
 
+  function initialize() {
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+    };
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          mapOptions.center.lat = position.coords.latitude;
+          mapOptions.center.lng = position.coords.longitude;
+          setPosition(mapOptions);
+        });
+    } else {
+        setPosition(mapOptions);
+    }
+  }
+
+  initialize();
 });
